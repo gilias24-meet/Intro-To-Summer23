99584ad59415e4d095ea0b6136ade72b6fcc9e14
gilias24-meet <ashelgal@gmail.com>
2024-07-14 20:53:22 +0300

    new

--- python_review.py
+++ python_review.py
@@ -3,6 +3,15 @@ temperatures=[]
 for i in range(7):
 	temperatures.append(random.randint(26,40))
 print(temperatures)
+days_of_the_week=["Sunday","Monday","tuesday" "wednesday" "thursday" "friday" "saturday"]
+for i in range(7):
+	temperatures.append(random.randint(26,40))
+	even=[]
+	if i % 2==0:
+		even.append(i)
+		
+
+
 
 
 

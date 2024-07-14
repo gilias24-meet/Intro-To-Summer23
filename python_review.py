487420e7c46ee54bf9623f7cc1af497c446b0295
gilias24-meet <ashelgal@gmail.com>
2024-07-14 20:20:26 +0300

    new

--- python_review.py
+++ python_review.py
@@ -1,7 +1,8 @@
 import random
-temperatures=[0,1,2,3,4,5,6]
+temperatures=[]
 for i in range(7):
-	temperatures[0:1:2:3:4:5:6] = [random.randint(26,41)]
+	temperatures[i] = [random.randint(26,41)]
 print(temperatures)
+
 
 

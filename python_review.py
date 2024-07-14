772a632802fe40f7eb3237ee0442ffe5020e906e
gilias24-meet <ashelgal@gmail.com>
2024-07-14 23:47:45 +0300

    new

--- python_review.py
+++ python_review.py
@@ -3,12 +3,62 @@ temperatures=[]
 for i in range(7):
 	temperatures.append(random.randint(26,40))
 print(temperatures)
+
 days_of_the_week=["Sunday","Monday","tuesday" "wednesday" "thursday" "friday" "saturday"]
-num=0
+good_days=0
 for i in temperatures:
 	if i % 2==0:
-    num=1+num
-print(num)
+		good_days=1+good_days
+print(good_days)
+
+high_temp=max(temperatures)
+low_temp=min(temperatures)
+high_temp_day = days_of_the_week[temperatures.index(high_temp)]
+low_temp_day = days_of_the_week[temperatures.index(low_temp)]
+
+
+# avg_temp = sum(temp) / len(temp)
+# above_avg = [temperatures for temperatures if temperatures > avg_temp]
+
+
+
+
+# the_index=temperatures.index(low_temp)
+# DL=days_of_the_week[the_index]
+# DH=days_of_the_week[temperatures.index(high_temp)]
+# print("high:", high_temp,DH)
+# print("low:", low_temp,DL)
+
+
+
+
+
+
+
+
+
+
+# high = 0
+# low=0
+# for i in range(7):
+# 	if temperatures[i] > temperatures[high]:
+# 		high=i
+
+# 	elif temperatures[i] < temperatures[low]:
+# 		low=i
+
+# # low_1=temperatures.index(low)
+# # print(low_1)
+
+# print("low:", low, days_of_the_week[low_1])
+
+# print("high:", high, days_of_the_week[high])
+
+
+
+
+
+
 
 
 

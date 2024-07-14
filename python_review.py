84d61f351a953afb39cf0ed9ae7d1d76458f9d5d
gilias24-meet <ashelgal@gmail.com>
2024-07-14 17:06:54 +0300

    new

--- python_review.py
+++ python_review.py
@@ -1,2 +1,7 @@
 import random
-temperatures=[6(random.randrange(26, 41))]:
+temperatures=[0,1,2,3,4,5,6]:
+for i in range(6):
+  n = (random.randrange(26, 41))
+  thislist[0] = n
+  n=n
+print temperatures:

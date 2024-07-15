0717229e6a1e498bd3be8dc8e864a82af671b4ed
gilias24-meet <ashelgal@gmail.com>
2024-07-15 22:45:37 +0300

    new

---
+++ pythonReview.py
@@ -0,0 +1,10 @@
+likes=0
+dislikes=0
+comments=0
+def create_youtube_video(title,description):
+
+	new_video = {"Title":title,"Description":description,"Likes":likes,"Dislikes":dislikes,"Username":comments}
+	likes = input("likes?")
+	dislikes = input("dislikes?")
+	comments = input("comments?")
+	create_youtube_video()

95a63774fbe050bad99016127b8f1f7d0776b9ed
gilias24-meet <ashelgal@gmail.com>
2024-07-16 09:44:29 +0300

    nee

--- pythonReview.py
+++ pythonReview.py
@@ -1,10 +1,15 @@
-likes=0
-dislikes=0
-comments=0
-def create_youtube_video(title,description):
+likes = input("likes?")
+dislikes = input("dislikes?")
+comments = input("comments?")
+create_youtube_video()
 
-	new_video = {"Title":title,"Description":description,"Likes":likes,"Dislikes":dislikes,"Username":comments}
-	likes = input("likes?")
-	dislikes = input("dislikes?")
-	comments = input("comments?")
-	create_youtube_video()
+
+
+def create_youtube_video(title,description):
+	new_video = {"Title":title,"Description":description,'Likes':likes,"Dislikes":dislikes,"Username":comment}
+	return new_video
+create_youtube_video("title","description")
+	
+
+
+	
